18c8aa806a578279b6682938056533b465735e19
Aaron Hobson <aaron@ahob85.com>
2021-08-19 10:34:16 -0700

    created assignment

---
+++ LoopsChallenge.java
@@ -0,0 +1,79 @@
+
+/**
+ * Write a description of class LoopsChallenge here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class LoopsChallenge
+{
+    /**
+     * Given a string and a non-negative integer n, return a larger string 
+     * that is n copies of the original string.
+     * 
+     * Examples:
+     * stringTimes("Hi", 2) → "HiHi"
+     * stringTimes("Hi", 3) → "HiHiHi"
+     * stringTimes("Hi", 1) → "Hi"
+     */
+    public static String stringTimes(String str, int n) {
+ 
+    }
+
+    /**
+     * Count the number of "xx" in the given string. We'll say that 
+     * overlapping is allowed, so "xxx" contains 2 "xx".
+     * 
+     * Examples:
+     * countXX("abcxx") → 1
+     * countXX("xxx") → 2
+     * countXX("xxxx") → 3
+     */
+    public static int countXX(String str) {
+
+    }
+    
+    /**
+     * Return true if the given string contains a "bob" string, but where the 
+     * middle 'o' char can be any char.
+     * 
+     * Examples:
+     * bobThere("abcbob") → true
+     * bobThere("b9b") → true
+     * bobThere("bac") → false
+     */
+    public static boolean bobThere(String str) {
+
+    }
+
+    /**
+     * Return the number of times that the string "code" appears anywhere in 
+     * the given string, except we'll accept any letter for the 'd', so 
+     * "cope" and "cooe" count.
+     * 
+     * Examples:
+     * countCode("aaacodebbb") → 1
+     * countCode("codexxcode") → 2
+     * countCode("cozexxcope") → 2
+     */
+    public static int countCode(String str) {
+
+    }
+
+    /**
+     * We'll say that a string is xy-balanced if for all the "x" characters 
+     * in the string, there exists a "y" character somewhere later in the 
+     * string. So "xxy" is balanced, but "xyx" is not. One "y" can balance 
+     * multiple "x"s. A string without any "x"s is balanced by default, since 
+     * there are no "x"s to unbalance it. Return true if the given string is 
+     * xy-balanced.
+     * 
+     * Examples:
+     * xyBalance("aaxbby") → true
+     * xyBalance("aaxbb") → false
+     * xyBalance("bbb") → true
+     */
+    public static boolean xyBalance(String str) {
+
+    }
+}
---
+++ LoopsChallengeTest.java
@@ -0,0 +1,126 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class LoopsChallengeTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class LoopsChallengeTest
+{
+    /**
+     * Default constructor for test class LoopsChallengeTest
+     */
+    public LoopsChallengeTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void stringTimesTest() {
+        assertEquals("HiHi", LoopsChallenge.stringTimes("Hi", 2));
+        assertEquals("HiHiHi", LoopsChallenge.stringTimes("Hi", 3));
+        assertEquals("Hi", LoopsChallenge.stringTimes("Hi", 1));
+        assertEquals("", LoopsChallenge.stringTimes("Hi", 0));
+        assertEquals("HiHiHiHiHi", LoopsChallenge.stringTimes("Hi", 5));
+        assertEquals("Oh Boy!Oh Boy!", LoopsChallenge.stringTimes("Oh Boy!", 2));
+        assertEquals("xxxx", LoopsChallenge.stringTimes("x", 4));
+        assertEquals("", LoopsChallenge.stringTimes("", 4));
+        assertEquals("codecode", LoopsChallenge.stringTimes("code", 2));
+        assertEquals("codecodecode", LoopsChallenge.stringTimes("code", 3));
+    }
+    
+    @Test
+    public void countXXTest() {
+        assertEquals(1, LoopsChallenge.countXX("abcxx"));
+        assertEquals(2, LoopsChallenge.countXX("xxx"));
+        assertEquals(3, LoopsChallenge.countXX("xxxx"));
+        assertEquals(0, LoopsChallenge.countXX("abc"));
+        assertEquals(0, LoopsChallenge.countXX("Hello there"));
+        assertEquals(2, LoopsChallenge.countXX("Hexxo thxxe"));
+        assertEquals(0, LoopsChallenge.countXX(""));
+        assertEquals(0, LoopsChallenge.countXX("Kittens"));
+        assertEquals(2, LoopsChallenge.countXX("Kittensxxx"));
+    }
+    
+    @Test
+    public void bobThereTest() {
+        assertEquals(true, LoopsChallenge.bobThere("abcbob"));
+        assertEquals(true, LoopsChallenge.bobThere("b9b"));
+        assertEquals(false, LoopsChallenge.bobThere("bac"));
+        assertEquals(true, LoopsChallenge.bobThere("bbb"));
+        assertEquals(false, LoopsChallenge.bobThere("abcdefb"));
+        assertEquals(true, LoopsChallenge.bobThere("123abcbcdbabxyz"));
+        assertEquals(false, LoopsChallenge.bobThere("b12"));
+        assertEquals(true, LoopsChallenge.bobThere("b1b"));
+        assertEquals(true, LoopsChallenge.bobThere("b12b1b"));
+        assertEquals(false, LoopsChallenge.bobThere("bbc"));
+        assertEquals(true, LoopsChallenge.bobThere("bbb"));
+        assertEquals(false, LoopsChallenge.bobThere("bb"));
+        assertEquals(false, LoopsChallenge.bobThere("b"));
+    }
+
+    @Test
+    public void countCodeTest() {
+        assertEquals(1, LoopsChallenge.countCode("aaacodebbb"));
+        assertEquals(2, LoopsChallenge.countCode("codexxcode"));
+        assertEquals(2, LoopsChallenge.countCode("cozexxcope"));
+        assertEquals(1, LoopsChallenge.countCode("cozfxxcope"));
+        assertEquals(1, LoopsChallenge.countCode("xxcozeyycop"));
+        assertEquals(0, LoopsChallenge.countCode("cozcop"));
+        assertEquals(0, LoopsChallenge.countCode("abcxyz"));
+        assertEquals(1, LoopsChallenge.countCode("code"));
+        assertEquals(0, LoopsChallenge.countCode("ode"));
+        assertEquals(0, LoopsChallenge.countCode("c"));
+        assertEquals(0, LoopsChallenge.countCode(""));
+        assertEquals(3, LoopsChallenge.countCode("AAcodeBBcoleCCccoreDD"));
+        assertEquals(2, LoopsChallenge.countCode("AAcodeBBcoleCCccorfDD"));
+        assertEquals(3, LoopsChallenge.countCode("coAcodeBcoleccoreDD"));
+    }
+
+    @Test
+    public void testXyBalance() {
+        assertEquals(true, LoopsChallenge.xyBalance("aaxbby"));
+        assertEquals(false, LoopsChallenge.xyBalance("aaxbb"));
+        assertEquals(false, LoopsChallenge.xyBalance("yaaxbb"));
+        assertEquals(true, LoopsChallenge.xyBalance("yaaxbby"));
+        assertEquals(true, LoopsChallenge.xyBalance("xaxxbby"));
+        assertEquals(false, LoopsChallenge.xyBalance("xaxxbbyx"));
+        assertEquals(true, LoopsChallenge.xyBalance("xxbxy"));
+        assertEquals(false, LoopsChallenge.xyBalance("xxbx"));
+        assertEquals(true, LoopsChallenge.xyBalance("bbb"));
+        assertEquals(false, LoopsChallenge.xyBalance("bxbb"));
+        assertEquals(true, LoopsChallenge.xyBalance("bxyb"));
+        assertEquals(true, LoopsChallenge.xyBalance("xy"));
+        assertEquals(true, LoopsChallenge.xyBalance("y"));
+        assertEquals(false, LoopsChallenge.xyBalance("x"));
+        assertEquals(true, LoopsChallenge.xyBalance(""));
+        assertEquals(false, LoopsChallenge.xyBalance("yxyxyxyx"));
+        assertEquals(true, LoopsChallenge.xyBalance("yxyxyxyxy"));
+        assertEquals(true , LoopsChallenge.xyBalance("12xabxxydxyxyzz"));
+    }
+}
